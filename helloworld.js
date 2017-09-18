1a7a5cf20e6e05dbbf4fa6613e75866f60108104
zhengjin <zivieira@163.com>
2017-09-18 14:25:34 +0800

    Update helloworld demo.

--- helloworld.js
+++ helloworld.js
@@ -6,8 +6,13 @@
 var testPath = function () {
     const path = require('path');
 
-    console.log(__dirname);
-    console.log(path.resolve(__dirname, '../screenshot'));
+    // path.join concatenates all given path segments together 
+    // using the platform specific separator as a delimiter, then normalizes the resulting path.
+    console.log('path.join:', path.join(__dirname, '../logs'));
+
+    // path.resolve() process the sequence of paths from right to left, 
+    // with each subsequent path prepended until an absolute path is constructed.
+    console.log('path.resolve:', path.resolve(__dirname, '../logs', '/bar/bae'));
 }
 
 if (require.main === module) {
