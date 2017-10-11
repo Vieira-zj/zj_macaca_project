b49212f1f0993b984cd0617c8900f64bef9e5b3b
zhengjin <zivieira@163.com>
2017-10-11 10:47:06 +0800

    Add async demos.

---
+++ mocha-test/demo03_async.js
@@ -0,0 +1,94 @@
+/**
+ * Asnyc functions to sync.
+ * http://www.cnblogs.com/cpselvis/p/6344122.html
+ * 
+ */
+
+const asyncByCallback1 = function () {
+  setTimeout(function () {
+    console.log('sleep 3000 ms');
+    setTimeout(function () {
+      console.log('sleep 2000 ms');
+      setTimeout(function () {
+        console.log('sleep 1000 ms');
+      }, 1000);
+    }, 2000);
+  }, 3000);
+}
+
+const asyncByCallback2 = function () {
+  let asyncFn = function (wait) {
+    if (wait < 1000) {
+      console.warn('wait time less than 1000ms!');
+      return;
+    }
+
+    setTimeout(function () {
+      console.log(`sleep ${wait} ms`);
+      asyncFn(wait - 1000);
+    }, wait);
+  };
+
+  asyncFn(3000);
+};
+
+const asyncByPromise = function () {
+  let asyncFn = function (wait) {
+    return new Promise(function (resolve, reject) {
+      if (wait < 1000) {
+        reject('wait time less than 1000ms!');
+      }
+
+      setTimeout(function () {
+        console.log(`sleep ${wait} ms`);
+        resolve(wait - 1000);
+      }, wait);
+    });
+  };
+
+  asyncFn(3000)
+    .then(asyncFn)
+    .then(asyncFn)
+    .then(asyncFn)
+    .catch(reason => console.warn(reason));
+};
+
+const asyncByAwait = function () {
+  let asyncFn = function (wait) {
+    return new Promise(function (resolve, reject) {
+      if (wait < 1000) {
+        reject('wait time less than 1000ms!');
+      }
+
+      setTimeout(function () {
+        console.log(`sleep ${wait} ms`);
+        resolve(wait - 1000);
+      }, wait);
+    });
+  };
+
+  async function asyncTest() {
+    try {
+      let timeout = await asyncFn(3000);
+      timeout = await asyncFn(timeout);
+      timeout = await asyncFn(timeout);
+      timeout = await asyncFn(timeout);
+      console.log('timeout after execution:', timeout);
+    } catch (err) {
+      console.warn(err);
+    }
+  }
+
+  asyncTest();
+};
+
+if (require.main === module) {
+  // asyncByCallback1();
+  // asyncByCallback2();
+
+  // asyncByPromise();
+
+  asyncByAwait();
+
+  console.log('async functions test.');
+}
