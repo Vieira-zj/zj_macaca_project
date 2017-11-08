fb5784eaa860a64ff75a60efd4eb9ba2bb5c183a
zhengjin <zivieira@163.com>
2017-11-08 17:47:00 +0800

    Add demo.

--- mocha-test/demo05_benchmark.js
+++ mocha-test/demo05_benchmark.js
@@ -25,6 +25,36 @@ let example01 = function () {
     });
 }
 
+let example02 = function () {
+  let suite = new Benchmark.Suite;
+  let tmpNum = '100';
+
+  let int1 = function (str) {
+    return +str;
+  }
+  let int2 = function (str) {
+    return Number(str);
+  }
+  let int3 = function (str) {
+    return parseInt(str, 10);
+  }
+
+  suite
+    .add('+', () => int1(tmpNum))
+    .add('Number', () => int2(tmpNum))
+    .add('parseInt', () => int3(tmpNum))
+    .on('cycle', function (event) {
+      console.log(String(event.target));
+    })
+    .on('complete', function () {
+      console.log('Fastest is', this.filter('fastest').map('name'));
+    })
+    .run({
+      'async': true
+    });
+}
+
 if (require.main === module) {
-  example01();
+  // example01();
+  example02();
 }
